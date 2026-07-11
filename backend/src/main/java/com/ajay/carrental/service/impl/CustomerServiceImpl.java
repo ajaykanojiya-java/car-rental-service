2a72fac0048390be40c7a973c06f04c263929340
Ajay Kanojiya <kanojiyaajay22@gmail.com>
2026-07-11 14:53:08 +0530

    Phase-5 Customer Service implementation

---
+++ backend/src/main/java/com/ajay/carrental/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ajay.carrental.service.impl;
+
+import com.ajay.carrental.entity.Customer;
+import com.ajay.carrental.repository.CustomerRepository;
+import com.ajay.carrental.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public Customer getOrCreateCustomer(String name, int licenseYears) {
+
+        return customerRepository.findByName(name)
+                .orElseGet(() -> createCustomer(name, licenseYears));
+    }
+
+    private Customer createCustomer(String name, int licenseYears) {
+
+        Customer customer = Customer.builder()
+                .name(name)
+                .licenseIssueDate(LocalDate.now().minusYears(licenseYears))
+                .build();
+        return customerRepository.save(customer);
+    }
+}
